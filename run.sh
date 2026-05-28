e06d85cd291bd4b745bd67d24ab38cc7a1477471
Biswajeet Rout <biswyaa@Biswajeets-MacBook-Air.local>
2026-05-28 16:20:02 +0530

    final

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -e
+cd "$(dirname "$0")/dont_touch"
+[ ! -L web ] && ln -sf ../web web
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release >/dev/null 2>&1
 cmake --build build 2>/dev/null
-./build/railway_app
+exec ./build/railway_app
